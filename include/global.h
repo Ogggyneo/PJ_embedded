96f7e63f01c07c7c78474241646c605e5aad1c20
LiL Nat <lilnat@LiLs-MacBook-Pro.local>
2025-10-24 15:00:40 +0700

    Update webserver UI, add OTA feature and RS485 task

--- include/global.h
+++ include/global.h
@@ -6,14 +6,15 @@
 #include "freertos/task.h"
 #include "freertos/semphr.h"
 
-
 extern float glob_temperature;
 extern float glob_humidity;
 
-extern String ssid;
-extern String password;
-extern String wifi_ssid;
-extern String wifi_password;
+extern String WIFI_SSID;
+extern String WIFI_PASS;
+extern String CORE_IOT_TOKEN;
+extern String CORE_IOT_SERVER;
+extern String CORE_IOT_PORT;
+
 extern boolean isWifiConnected;
 extern SemaphoreHandle_t xBinarySemaphoreInternet;
 #endif
